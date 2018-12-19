0c5c2dcf7b53f88fbf157831ef1326844af79f53
LeoooY <Leoyy3@163.com>
2018-12-19 18:02:45 +0800

    hashSearch

---
+++ Algorithms/JSBinarySearch.js
@@ -0,0 +1,28 @@
+/*
+    Js实现二分查找
+*/
+let OrderList=[1,3,9,17,29,45,50,70,90];
+function BinarySearch(OrderList,elem){
+    let high=OrderList.length-1;
+    let low = 0;
+    let mid;
+    while(low<high){
+        mid=Math.round((high+low)/2);
+        if(OrderList[mid]===elem){
+            return mid;
+        }
+
+        if(elem>OrderList[mid]){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+        
+        
+    }
+    return -1;
+}
+
+let rs=BinarySearch(OrderList,51);
+console.log(rs,OrderList[rs]);
